65e7b14f5076333f44c3cced3fe3ff1943319470
Cristiano Fromagio <fromagio_cristiano@live.com>
2016-08-25 17:18:44 -0300

    Updating content displacement and assembled page.

--- public/dist/js/rotasul.js
+++ public/dist/js/rotasul.js
@@ -6,6 +6,160 @@
 
 
 
+/* Contact */
+
+var Contact = (function () {
+
+	/**
+	 * Contact constructor
+	 * @constructor
+	 */
+	function Contact() {
+
+		/* Contact email manipulation */
+
+		var formSentCount = 0;
+		var formSentCountLimit = 2;
+
+		var requestURL = 'http://mailman.letsmowe.com/citodon/';
+		var formLocked = false;
+
+		var form = {
+			viewport: document.getElementById('cForm')
+		};
+
+		form.fields = {};
+		form.sendButton = {};
+
+		form.fields.cName = document.getElementById('cName');
+		form.fields.cCity = document.getElementById('cCity');
+		form.fields.cPhone = document.getElementById('cPhone');
+		form.fields.cEmail = document.getElementById('cEmail');
+		form.fields.cMessage = document.getElementById('cMessage');
+		form.sendButton.viewport = document.getElementById('cSubmit');
+		// form.fields.cAddress = document.getElementById('cAddress');
+
+		form.states = [
+			'is-error',
+			'is-fail',
+			'is-sending',
+			'is-success'
+		];
+
+		form.changeState = function (state) {
+
+			if (form.viewport) {
+
+				for (var i = form.states.length; i--; )
+					form.viewport.classList.remove(form.states[i])
+
+				form.viewport.classList.add(state);
+
+			}
+
+		};
+
+		// send the ajax request
+		form.sendRequest = function(requestData) {
+
+			if (requestData) {
+
+				// ajax request
+				$.ajax({
+					cache: false,
+					crossDomain: true,
+					data: requestData,
+					method: 'get',
+					beforeSend: function() {
+						formLocked = true;
+						form.changeState('is-sending');
+					},
+					error: function (data) {
+						console.log(data);
+						form.changeState('is-fail');
+						form.send(requestData, 5000);
+					},
+					success: function (data) {
+						console.log(data);
+						if (data.sent)
+							form.changeState('is-success');
+						else
+							form.changeState('is-error');
+						formLocked = false;
+					},
+					timeout: 12000,
+					url: requestURL
+				});
+
+			}
+
+		};
+
+		// control the time delay to init the ajax request
+		form.send = function(requestData, delay) {
+
+			if (requestData) {
+
+				if (delay) {
+					setTimeout(function() {
+						form.sendRequest(requestData);
+					}, delay)
+				} else {
+					form.sendRequest(requestData);
+				}
+
+			}
+
+		};
+
+		// form submit button listener
+		form.sendButton.viewport.addEventListener('click', function (ev) {
+
+			ev.preventDefault();
+
+			if (!formLocked) {
+
+				if (formSentCount < formSentCountLimit) {
+
+					var allow = !!(form.fields.cName.value && (form.fields.cPhone.value || form.fields.cEmail.value) && form.fields.cCity.value && form.fields.cMessage.value);
+
+					if (allow) {
+
+						// lock the form
+						formLocked = true;
+
+						// count the request
+						formSentCount++;
+
+						// get object data
+						var requestData = {
+							cName: form.fields.cName.value,
+							cPhone: form.fields.cPhone.value,
+							cEmail: form.fields.cEmail.value,
+							cAddress: "",
+							cCity: form.fields.cCity.value,
+							cMessage: form.fields.cMessage.value
+						};
+
+						// send
+						form.send(requestData, false);
+
+					} else {
+						form.changeState('is-error');
+					}
+
+				}
+
+			}
+
+		});
+
+	}
+
+	return Contact;
+
+})();
+
 /* Mowe Logo 1.6 */
 
 var Logo = (function () {
@@ -89,5 +243,126 @@ var Logo = (function () {
 	};
 
 	return Logo;
+
+})();
+
+/* Required fields */
+
+var RequiredField = (function () {
+
+	/**
+	 * Required Field constructor
+	 * @constructor
+	 */
+	function RequiredField(viewport, fieldClass) {
+
+		var self = this;
+
+		this.viewport = viewport;
+
+		this.input = {};
+		this.fieldClass = fieldClass;
+
+		this.onClick = function () {
+
+			try {
+
+				self.input.viewport.focus();
+
+			} catch ( e ) { }
+
+		};
+
+		this.onFocus = function () {
+
+			self.viewport.classList.add('has-focus');
+
+			if (self.input.viewport.value)
+				self.viewport.classList.remove('is-empty');
+
+		};
+
+		this.onBlur = function () {
+
+			self.viewport.classList.remove('has-focus');
+
+			if (!self.input.viewport.value)
+				self.viewport.classList.add('is-empty');
+			else
+				self.viewport.classList.remove('is-empty');
+
+		};
+
+		this.onInput = function () {
+
+			if (self.input.viewport.value) {
+				self.viewport.classList.remove('is-empty');
+				self.viewport.classList.add('has-label');
+				self.viewport.classList.add('is-valid');
+			} else {
+				self.viewport.classList.remove('has-label');
+				self.viewport.classList.remove('is-valid');
+			}
+
+		};
+
+		if (this.viewport)
+			this.init();
+
+	}
+
+	/**
+	 * Normalize
+	 */
+	RequiredField.prototype.normalize = function () {
+
+		if (this.input.viewport.value)
+			this.viewport.classList.add('has-label');
+
+	};
+
+	/**
+	 * Add the listeners
+	 * It support IE8
+	 */
+	RequiredField.prototype.addListeners = function () {
+
+		try {
+
+			this.viewport.addEventListener('click', this.onClick, false);
+
+			this.input.viewport.addEventListener('focus', this.onFocus, false);
+			this.input.viewport.addEventListener('blur', this.onBlur, false);
+			this.input.viewport.addEventListener('input', this.onInput, false);
+
+		} catch ( e ) {	}
+
+	};
+
+	/**
+	 * Get the input element
+	 * @return {boolean}
+	 */
+	RequiredField.prototype.getInputElement = function () {
+
+		this.input.viewport = this.viewport.querySelector(this.fieldClass);
+
+		return !!this.input.viewport;
+
+	};
+
+	/**
+	 * Init the instance
+	 */
+	RequiredField.prototype.init = function () {
+
+		if (this.getInputElement())
+			this.addListeners();
+
+		this.normalize();
+
+	};
+
+	return RequiredField;
 
 })();
